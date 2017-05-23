8b4cf53843c6b4eba0c1860a760f231ac98c0be1
DinaMusalam <dinaeid@yahoo.com>
2017-05-23 16:07:14 +0200

    Show properties

--- storage.js
+++ storage.js
@@ -1,5 +1,7 @@
 var request = require('request');
-var config = require('./config')
+var config = require('./config');
+const util = require('util')
+
 
 var apiUrl = config.apiUrl();
 
@@ -23,7 +25,7 @@ module.exports = {
             request(options, function (error, response, data) {
                 if (!error && response.statusCode == 200) {
                     var str = JSON.parse(data); 
-                    console.log('the storage name is =' + str);
+                    console.log('the storage name is =' + util.inspect(str));
                     resolve(str);
                 } else {
                      console.log('error storage name is =' + error);
@@ -34,27 +36,27 @@ module.exports = {
     },
     
 
-    storageArtifactLevel: function(repo , token) {
-        return new Promise((resolve, reject) => {
-            // Set the headers
-            const headers = {
-                'X-JFrog-Art-Api': token
-            }
+    // storageArtifactLevel: function(repo , token) {
+    //     return new Promise((resolve, reject) => {
+    //         // Set the headers
+    //         const headers = {
+    //             'X-JFrog-Art-Api': token
+    //         }
 
-            // Configure the request
-            const options = {
-                url: apiUrl + 'storage/' + repo ,
-                method: 'GET',
-                headers: headers,
-            }
+    //         // Configure the request
+    //         const options = {
+    //             url: apiUrl + 'storage/' + repo ,
+    //             method: 'GET',
+    //             headers: headers,
+    //         }
 
-            // Start the request
-            request(options, function (error, response, data) {
-                if (!error && response.statusCode == 200) {
-                    var str = JSON.parse(data); 
-                    resolve(str);
-                }
-            })
-        })
-    }
+    //         // Start the request
+    //         request(options, function (error, response, data) {
+    //             if (!error && response.statusCode == 200) {
+    //                 var str = JSON.parse(data); 
+    //                 resolve(str);
+    //             }
+    //         })
+    //     })
+    // }
 }
